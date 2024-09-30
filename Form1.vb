4b4cf2035cb64959bd3790443751140ad17a595c
Wekideronnie-2000 <wekideronnie@gmail.com>
2024-09-30 15:17:40 +0300

    Grade Scale Calculator

---
+++ Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Public Property TextBoxGrade As Object
+    Public Property TextBoxMarks As Object
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnEvaluate_Click(sender As Object, e As EventArgs) Handles btnEvaluate.Click
+        Dim marks As Integer
+        marks = CInt(txtMarks.Text)
+
+        If marks > 100 Then
+            MessageBox.Show("Wrong entry, please re-enter the mark")
+            txtMarks.Clear()
+        ElseIf marks >= 90 Then
+            txtGrade.Text = "Excellent"
+        ElseIf marks >= 80 Then
+            txtGrade.Text = "Very Good"
+        ElseIf marks >= 70 Then
+            txtGrade.Text = "Good"
+        ElseIf marks >= 60 Then
+            txtGrade.Text = "Medium"
+        ElseIf marks >= 50 Then
+            txtGrade.Text = "Pass"
+        Else
+            txtGrade.Text = "Fail"
+        End If
+    End Sub
+
+    Private Sub txtMarks_TextChanged(sender As Object, e As EventArgs) Handles txtMarks.TextChanged
+
+    End Sub
+
+    Private Sub txtGrade_TextChanged(sender As Object, e As EventArgs) Handles txtGrade.TextChanged
+
+    End Sub
+End Class
